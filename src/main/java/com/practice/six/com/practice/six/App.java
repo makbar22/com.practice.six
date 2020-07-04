74644bafc203b0fabc5b50d2d1468af02a095763
Murtaza Akbar <makbar22>
2020-07-03 22:32:37 -0400

    practice six

---
+++ src/main/java/com/practice/six/com/practice/six/App.java
@@ -0,0 +1,13 @@
+package com.practice.six.com.practice.six;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/practice/six/com/practice/six/AppTest.java
@@ -0,0 +1,20 @@
+package com.practice.six.com.practice.six;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
